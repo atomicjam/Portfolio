652bdc45fbb0aa9fba90c259444883ffb41225ed
Elliot Goode <atomicjam@gmail.com>
2023-03-30 16:13:01 +0100

    added workbox

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,23 @@
 // vite.config.js
 import { resolve } from 'path'
 import { defineConfig } from 'vite'
+import { VitePWA } from "vite-plugin-pwa"
 
 export default defineConfig({
+    plugins: [
+        VitePWA({      
+                manifest: { 
+                  icons: [
+                    {
+                      src: "/icons/512.png",
+                      sizes: "512x512",
+                      type: "image/png",
+                      purpose: "any maskable"
+                    }
+                  ]
+                }
+              })
+    ],
     build: {
         rollupOptions: {
             input: {
